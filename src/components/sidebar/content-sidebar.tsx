63f5cc1196d27b9f8c903047fb3a483686d7bcc7
Pham Hong Son <sonph@itelecom.vn>
2024-11-26 11:17:17 +0700

    fix

--- src/components/sidebar/content-sidebar.tsx
+++ src/components/sidebar/content-sidebar.tsx
@@ -27,7 +27,7 @@ function ContentSidebar() {
 
     return (
         <>
-            <div className={cn("flex justify-end p-4")}>
+            <div className="flex justify-end p-4">
                 <Popover>
                     <PopoverTrigger>
                         <Avatar className="h-10 w-10 md:h-12 md:w-12">
@@ -68,7 +68,7 @@ function ContentSidebar() {
             <div className={cn("absolute flex gap-2 top-4 z-10", isMobile ? "flex-row" : "flex-col")}>
                 {
                     (!open || isMobile) &&
-                    <div className="md:fixed md:bottom-4 flex md:flex-col gap-2 md:left-4">
+                    <div className="pl-2 md:fixed md:bottom-4 flex md:flex-col gap-2 md:left-4">
                         <TooltipProvider delayDuration={0}>
                             <Tooltip>
                                 <TooltipTrigger>
