b2ff663c2b90606d502535e674fef06cd1367c5f
Pham Hong Son <sonph@itelecom.vn>
2024-12-06 17:03:10 +0700

    fix

--- src/pages/temp/index.tsx
+++ src/pages/temp/index.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 function Temp() {
     const { id } = useParams();
 
-    const link = `https://kiosk.xbot.vn/api/public/pa/image/${id}.jpg`
+    const link = `https://kiosk.xbot.vn/api/public/pa/image/${id}`
 
     return (
         <img src={link} className="w-screen"/>
